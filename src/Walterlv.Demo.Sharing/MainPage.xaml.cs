494b75f3245f1642907a448cc35642d4bd688f8b
walterlv <xblero@gmail.com>
2017-10-25 23:57:44 +0800

    Add random storyboard page.

--- src/Walterlv.Demo.Sharing/MainPage.xaml.cs
+++ src/Walterlv.Demo.Sharing/MainPage.xaml.cs
@@ -1,7 +1,12 @@
 ﻿#if WINDOWS_UWP
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Walterlv.Demo.Pages;
 #else
+using System;
+using System.Windows;
 using System.Windows.Controls;
+using Walterlv.Demo.Pages;
 #endif
 
 namespace Walterlv.Demo
@@ -10,7 +15,18 @@ namespace Walterlv.Demo
     {
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            Loaded -= OnLoaded;
+#if WINDOWS_UWP
+            MainFrame.Navigate(typeof(StoryboardPage));
+#else
+            MainFrame.Navigate(new StoryboardPage());
+#endif
         }
     }
 }
--- src/Walterlv.Demo.Sharing/Pages/StoryboardPage.xaml.cs
+++ src/Walterlv.Demo.Sharing/Pages/StoryboardPage.xaml.cs
@@ -1,7 +1,15 @@
 ﻿#if WINDOWS_UWP
+using System;
+using Windows.Foundation;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+
 #else
+using System;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
 #endif
 
 namespace Walterlv.Demo.Pages
@@ -11,6 +19,52 @@ namespace Walterlv.Demo.Pages
         public StoryboardPage()
         {
             InitializeComponent();
+            Loaded += OnLoaded;
+        }
+
+#if !WINDOWS_UWP
+        private Storyboard TranslateStoryboard => (Storyboard) FindResource("Storyboard.Translate");
+#endif
+
+        private readonly Random _random = new Random(DateTime.Now.Ticks.GetHashCode());
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            Loaded -= OnLoaded;
+            TranslateStoryboard.Begin();
+            TranslateStoryboard.Stop();
+        }
+
+        private void BeginStoryboard_Click(object sender, RoutedEventArgs e)
+        {
+            TranslateStoryboard.Begin();
+            MoveToRandomPosition();
+        }
+
+        private void BeginStoryboard2_Click(object sender, RoutedEventArgs e)
+        {
+            MoveToRandomPosition();
+            TranslateStoryboard.Begin();
+            MoveToRandomPosition();
+        }
+
+        private void PauseStoryboard_Click(object sender, RoutedEventArgs e)
+        {
+            TranslateStoryboard.Pause();
+        }
+
+        private void MoveToRandomPosition()
+        {
+            var nextPosition = NextRandomPosition();
+            TranslateTransform.X = nextPosition.X;
+            TranslateTransform.Y = nextPosition.Y;
+        }
+
+        private Point NextRandomPosition()
+        {
+            var areaX = (int) Math.Round(DisplayCanvas.ActualWidth - DisplayShape.ActualWidth);
+            var areaY = (int) Math.Round(DisplayCanvas.ActualHeight - DisplayShape.ActualHeight);
+            return new Point(_random.Next(areaX) + 1, _random.Next(areaY) + 1);
         }
     }
 }
