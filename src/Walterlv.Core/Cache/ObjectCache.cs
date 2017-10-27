abe29746907ae1102bcff7ffe19aecea6e3bcf05
walterlv <xblero@gmail.com>
2017-10-27 11:55:43 +0800

    Add cache and other render method.

---
+++ src/Walterlv.Core/Cache/ObjectCache.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace Walterlv.Cache
+{
+    public class ObjectCache<TKey, TValue> where TValue : class
+    {
+        private static readonly Dictionary<TKey, WeakReference<TValue>> Cache = new Dictionary<TKey, WeakReference<TValue>>();
+
+        public Func<TKey, TValue> Converter { get; set; }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private TValue Convert(TKey key)
+        {
+            return Converter?.Invoke(key);
+        }
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                if (Cache.TryGetValue(key, out var reference))
+                {
+                    if (reference.TryGetTarget(out var value))
+                    {
+                        return value;
+                    }
+                }
+                var newValue = Convert(key);
+                reference = new WeakReference<TValue>(newValue);
+                Cache[key] = reference;
+                return newValue;
+            }
+        }
+    }
+}
---
+++ src/Walterlv.Core/Cache/ValueCache.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace Walterlv.Cache
+{
+    public class ValueCache<TKey, TValue> where TValue : struct
+    {
+        private static readonly Dictionary<TKey, TValue> Cache = new Dictionary<TKey, TValue>();
+
+        public Func<TKey, TValue> Converter { get; set; }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private TValue Convert(TKey key)
+        {
+            return Converter?.Invoke(key) ?? default(TValue);
+        }
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                if (Cache.TryGetValue(key, out var value)) return value;
+                value = Convert(key);
+                Cache.Add(key, value);
+                return value;
+            }
+        }
+    }
+}
