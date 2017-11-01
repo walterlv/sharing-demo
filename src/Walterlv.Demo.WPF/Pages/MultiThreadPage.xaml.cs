b32013bb500109552ad794933a536878f47e969b
walterlv <xblero@gmail.com>
2017-11-01 18:45:38 +0800

    Add background target.

--- src/Walterlv.Demo.WPF/Pages/MultiThreadPage.xaml.cs
+++ src/Walterlv.Demo.WPF/Pages/MultiThreadPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace Walterlv.Demo.Pages
 {
@@ -7,6 +10,24 @@ namespace Walterlv.Demo.Pages
         public MultiThreadPage()
         {
             InitializeComponent();
+            Loaded += OnLoaded;
+        }
+
+        private async void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            Loaded -= OnLoaded;
+
+            await TargetHost.SetChildAsync(() =>
+            {
+                var box = new TextBox
+                {
+                    Text = "吕毅 - walterlv",
+                    Width = 300,
+                    Height = 120,
+                    Background = new SolidColorBrush(Colors.White) {Opacity = 0.5},
+                };
+                return box;
+            });
         }
     }
 }
