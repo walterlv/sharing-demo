29a6b9295775c6f01a4426b7b930778ca69a8bba
walterlv <xblero@gmail.com>
2017-11-02 22:34:11 +0800

    Let Window in another thread.

--- src/Walterlv.Demo.WPF/Pages/MultiThreadPage.xaml.cs
+++ src/Walterlv.Demo.WPF/Pages/MultiThreadPage.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Walterlv.Demo.Pages
 {
@@ -28,6 +31,72 @@ namespace Walterlv.Demo.Pages
                 };
                 return box;
             });
+            
+
+            await TestLoadUnloadIssueAsync();
+        }
+
+        private async Task TestLoadUnloadIssueAsync()
+        {
+            var elementA = new TextBlock
+            {
+                Text = "A",
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+            };
+            var elementB = new TextBlock
+            {
+                Text = "B",
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+            };
+            var elementC = new Rectangle
+            {
+                Width = 40,
+                Height = 40,
+                Fill = new SolidColorBrush(Colors.Tomato) {Opacity = 0.5},
+            };
+            var groupD = new Grid();
+            groupD.Children.Add(elementB);
+            groupD.Children.Add(elementC);
+            LoadUnloadTestingPanel.Children.Add(elementA);
+            LoadUnloadTestingPanel.Children.Add(groupD);
+
+            for (var i = 0; i < 100; i++)
+            {
+                await Task.Delay(1000);
+                await Ungroup();
+                await Dispatcher.Yield(DispatcherPriority.Input);
+                await Group();
+            }
+
+            async Task Group()
+            {
+                var groupE = (Grid) LoadUnloadTestingPanel.Children[0];
+                LoadUnloadTestingPanel.Children.Remove(groupE);
+                await Dispatcher.Yield();
+                groupE.Children.Remove(elementA);
+                await Dispatcher.Yield();
+                LoadUnloadTestingPanel.Children.Add(elementA);
+                await Dispatcher.Yield();
+                groupE.Children.Remove(groupD);
+                await Dispatcher.Yield();
+                LoadUnloadTestingPanel.Children.Add(groupD);
+            }
+
+            async Task Ungroup()
+            {
+                var groupE = new Grid();
+                LoadUnloadTestingPanel.Children.Remove(elementA);
+                await Dispatcher.Yield();
+                groupE.Children.Add(elementA);
+                await Dispatcher.Yield();
+                LoadUnloadTestingPanel.Children.Remove(groupD);
+                await Dispatcher.Yield();
+                groupE.Children.Add(groupD);
+                await Dispatcher.Yield();
+                LoadUnloadTestingPanel.Children.Add(groupE);
+            }
         }
     }
 }
--- src/Walterlv.Demo.WPF/Utils/Threading/UIDispatcher.cs
+++ src/Walterlv.Demo.WPF/Utils/Threading/UIDispatcher.cs
@@ -67,5 +67,69 @@ namespace Walterlv.Demo
             thread.Start();
             return awaiter;
         }
+
+        /// <summary>
+        /// 创建一个可以运行 <see cref="Dispatcher"/> 的后台 UI 线程，并返回这个线程的调度器 <see cref="Dispatcher"/>。
+        /// </summary>
+        /// <param name="name">线程的名称，如果不指定，将使用 “BackgroundUI”。</param>
+        /// <returns>后台线程创建并启动的 <see cref="Dispatcher"/>。</returns>
+        public static Dispatcher RunNew([CanBeNull] string name = null)
+        {
+            var resetEvent = new AutoResetEvent(false);
+
+            // 记录原线程关联的 Dispatcher，以便在意外时报告异常。
+            var originDispatcher = Dispatcher.CurrentDispatcher;
+            Exception innerException = null;
+            Dispatcher dispatcher = null;
+
+            // 创建后台线程。
+            var thread = new Thread(() =>
+            {
+                try
+                {
+                    // 获取关联此后台线程的 Dispatcher。
+                    dispatcher = Dispatcher.CurrentDispatcher;
+
+                    // 设置此线程的 SynchronizationContext，以便此线程上 await 之后能够返回此线程。
+                    SynchronizationContext.SetSynchronizationContext(
+                        new DispatcherSynchronizationContext(dispatcher));
+
+                    // 报告 Dispatcher 已创建完毕，使用 ResetEvent 同步等待 Dispatcher 创建的地方可以继续执行了。
+                    resetEvent.Set();
+                }
+                catch (Exception ex)
+                {
+                    // 报告创建过程中发生的异常。
+                    innerException = ex;
+                    resetEvent.Set();
+                }
+
+                // 此线程的以下代码将脱离异步状态机的控制，需要自己处理异常。
+                try
+                {
+                    // 启动 Dispatcher，开始此线程上消息的调度。
+                    Dispatcher.Run();
+                }
+                catch (Exception ex)
+                {
+                    // 如果新的 Dispatcher 线程上出现了未处理的异常，则将其抛到原调用线程上。
+                    originDispatcher.InvokeAsync(() => ExceptionDispatchInfo.Capture(ex).Throw());
+                }
+            })
+            {
+                Name = name ?? "BackgroundUI",
+                IsBackground = true,
+            };
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+            resetEvent.WaitOne();
+            resetEvent.Dispose();
+            resetEvent = null;
+            if (innerException != null)
+            {
+                ExceptionDispatchInfo.Capture(innerException).Throw();
+            }
+            return dispatcher;
+        }
     }
 }
