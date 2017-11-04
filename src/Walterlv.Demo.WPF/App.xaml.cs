5e5ba51ce3e23fe2355055d95e797dd4110d2b03
walterlv <xblero@gmail.com>
2017-11-04 16:52:58 +0800

    Use traditional startup method.

--- src/Walterlv.Demo.WPF/App.xaml.cs
+++ src/Walterlv.Demo.WPF/App.xaml.cs
@@ -10,30 +10,30 @@ namespace Walterlv.Demo
             InitializeComponent();
         }
 
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            Frame rootFrame = CoreWindow.Current.Content as Frame;
+        //protected override void OnStartup(StartupEventArgs e)
+        //{
+        //    Frame rootFrame = CoreWindow.Current.Content as Frame;
 
-            // 不要在窗口已包含内容时重复应用程序初始化，
-            // 只需确保窗口处于活动状态
-            if (rootFrame == null)
-            {
-                // 创建要充当导航上下文的框架，并导航到第一页
-                rootFrame = new Frame();
+        //    // 不要在窗口已包含内容时重复应用程序初始化，
+        //    // 只需确保窗口处于活动状态
+        //    if (rootFrame == null)
+        //    {
+        //        // 创建要充当导航上下文的框架，并导航到第一页
+        //        rootFrame = new Frame();
 
-                // 将框架放在当前窗口中
-                CoreWindow.Current.Content = rootFrame;
-            }
+        //        // 将框架放在当前窗口中
+        //        CoreWindow.Current.Content = rootFrame;
+        //    }
 
-            if (rootFrame.Content == null)
-            {
-                // 当导航堆栈尚未还原时，导航到第一页，
-                // 并通过将所需信息作为导航参数传入来配置
-                // 参数
-                rootFrame.Navigate(new MainPage(), e.Args);
-            }
-            // 确保当前窗口处于活动状态
-            CoreWindow.Current.Activate();
-        }
+        //    if (rootFrame.Content == null)
+        //    {
+        //        // 当导航堆栈尚未还原时，导航到第一页，
+        //        // 并通过将所需信息作为导航参数传入来配置
+        //        // 参数
+        //        rootFrame.Navigate(new MainPage(), e.Args);
+        //    }
+        //    // 确保当前窗口处于活动状态
+        //    CoreWindow.Current.Activate();
+        //}
     }
 }
