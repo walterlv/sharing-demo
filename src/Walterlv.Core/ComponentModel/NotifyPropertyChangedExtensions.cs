1abd5ac9dd3b0485c0a538cff3b5d2dda440c2bc
walterlv <xblero@gmail.com>
2017-11-17 21:47:07 +0800

    Add notification object.

---
+++ src/Walterlv.Core/ComponentModel/NotifyPropertyChangedExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Walterlv.Annotations;
+
+namespace Walterlv.ComponentModel
+{
+    public abstract class NotificationObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetValue<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(field, value))
+            {
+                return false;
+            }
+
+            field = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            return true;
+        }
+    }
+}
---
+++ src/Walterlv.Demo.WPF/Pages/WindowMessagePage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Walterlv.Demo.Pages
+{
+    public partial class WindowMessagePage : Page
+    {
+        public WindowMessagePage()
+        {
+            InitializeComponent();
+            Loaded += OnLoaded;
+            Unloaded += OnUnloaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void OnUnloaded(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+
+    public sealed class WindowMessageViewModel
+    {
+        
+    }
+}
