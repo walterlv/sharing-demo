38ef6ce02ba124f869466c395b1f2969a6f7b4f4
walterlv <xblero@gmail.com>
2017-11-21 16:50:43 +0800

    Accurate the rotation matrix tracing.

--- src/Walterlv.Demo.Sharing/Pages/StoryboardPage.xaml.cs
+++ src/Walterlv.Demo.Sharing/Pages/StoryboardPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿#if WINDOWS_UWP
 using System;
 using System.Diagnostics;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -14,22 +15,21 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using MahApps.Metro.Converters;
+using Walterlv.ComponentModel;
 
 #endif
 
 namespace Walterlv.Demo.Pages
 {
-    public class SomePage<T> : Page
-    {
-        
-    }
-
     public partial class StoryboardPage
     {
         public StoryboardPage()
@@ -49,7 +49,7 @@ namespace Walterlv.Demo.Pages
 
         private readonly Random _random = new Random(DateTime.Now.Ticks.GetHashCode());
 
-        private void OnLoaded(object sender, RoutedEventArgs args)
+        private async void OnLoaded(object sender, RoutedEventArgs args)
         {
             Loaded -= OnLoaded;
             TranslateStoryboard.Begin();
@@ -57,9 +57,69 @@ namespace Walterlv.Demo.Pages
 
             for (var i = 0; i < DrawLineStoryboard.Children.Count; i++)
             {
-                InitializePathAndItsAnimation((Path) DisplayCanvas.Children[i + 1], (DoubleAnimation) DrawLineStoryboard.Children[i]);
+                InitializePathAndItsAnimation((Path) DisplayCanvas.Children[i + 2],
+                    (DoubleAnimation) DrawLineStoryboard.Children[i]);
             }
             DrawLineStoryboard.Begin();
+
+            while (true)
+            {
+                await Task.Delay(500);
+
+                var matrix = DisplayShape.RenderTransform.Value;
+                var (scaling, rotation, translation) = ExtractMatrix(matrix, (scalingFactor) =>
+                {
+                    var point = new Point(DisplayShape.ActualWidth, DisplayShape.ActualHeight);
+                    return (new Point(-(scalingFactor.X * point.X - 1) / 2, -(scalingFactor.Y * point.Y - 1) / 2),
+                        new Point((scalingFactor.X * point.X - 1) / 2, (scalingFactor.Y * point.Y - 1) / 2));
+                });
+                var group = new TransformGroup();
+                group.Children.Add(new ScaleTransform {ScaleX = scaling.X, ScaleY = scaling.Y});
+                //TraceShape.Width = DisplayShape.ActualWidth * scaling.X;
+                //TraceShape.Height = DisplayShape.ActualHeight * scaling.Y;
+                group.Children.Add(new RotateTransform {Angle = rotation});
+                group.Children.Add(new TranslateTransform {X = translation.X, Y = translation.Y});
+                TraceShape.RenderTransform = group;
+            }
+        }
+
+        private (Vector Scaling, double Rotation, Vector Translation) ExtractMatrix(
+            Matrix matrix, CenterSpecification specifyCenter = null)
+        {
+            var unitPoints = new[] {new Point(0, 0), new Point(1, 0), new Point(1, 1), new Point(0, 1)};
+            var transformedPoints = unitPoints.Select(matrix.Transform).ToArray();
+            var scaling = new Vector(
+                (transformedPoints[1] - transformedPoints[0]).Length,
+                (transformedPoints[3] - transformedPoints[0]).Length);
+            var rotation = Vector.AngleBetween(new Vector(1, 0), transformedPoints[1] - transformedPoints[0]);
+            var translation = transformedPoints[0] - unitPoints[0];
+            if (specifyCenter != null)
+            {
+                var (scalingCenter, rotationCenter) = specifyCenter(scaling);
+                // 如果目标自带缩放中心，那么此处需要先去掉缩放中心带来的平移分量。
+                translation += (new Vector());
+                // 如果目标自带旋转中心，那么此处需要先去掉旋转中心带来的平移分量。
+                translation += (Rotate(rotationCenter, rotation) - rotationCenter);
+            }
+            return (scaling, rotation, translation);
+        }
+
+        public delegate (Point ScalingCenter, Point RotationCenter) CenterSpecification(Vector scalingFactor);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private Point Rotate(Point point, double angle)
+        {
+            var matrix = Matrix.Identity;
+            matrix.Rotate(angle);
+            return matrix.Transform(point);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private Vector Rotate(Vector vector, double angle)
+        {
+            var matrix = Matrix.Identity;
+            matrix.Rotate(angle);
+            return matrix.Transform(vector);
         }
 
         private void InitializePathAndItsAnimation(Path path, DoubleAnimation animation)
@@ -149,10 +209,5 @@ namespace Walterlv.Demo.Pages
                 return Math.Sqrt(Math.Pow(p1.X - p2.X, 2) + Math.Pow(p1.Y - p2.Y, 2));
             }
         }
-    }
-
-    public class TestA : NotificationObject
-    {
-        
     }
 }
